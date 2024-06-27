37701fb2b34f32ace2d7361e20343546e88f5346
WeiWei <2183135126@qq.com>
2024-06-28 02:14:56 +0800

    1.1.0

--- yemian/zhuycounter.js
+++ yemian/zhuycounter.js
@@ -1,1 +1,50 @@
-export 
+import { marked } from 'marked';
+
+const markdownFilePath = '/biji/bijiweiz.json'; // 注意这里的路径可能是相对于构建后的根目录
+
+
+// 读取json文件篇数
+export function duqvmdwenjian() {
+    return fetch(markdownFilePath).then(response => response.json()).then(data => {
+        // console.log(data, 'data');
+        let count = 0;
+        for (const item of data) {
+            count++;
+        }
+        return count;
+    })
+}
+
+
+// 读取json文件内容名称
+export function duqvmdname() {
+    return fetch(markdownFilePath).then(response => response.json()).then(data => {
+
+        data.sort((a, b) => {
+            // 将字符串转换为 Date 对象进行比较  
+            const dateA = new Date(a.time);
+            const dateB = new Date(b.time);
+            // 返回结果决定了排序的方向，这里是升序排序  
+            return dateB - dateA;
+        });
+
+        let listElement = document.getElementById("wenzhang");
+        for (const item of data) {
+            let listItem = document.createElement('div');
+            let listTitle = document.createElement('h2');
+            let listDate = document.createElement('span')
+            let listintroduce = document.createElement('span')
+            listItem.className = 'wenbkuangjia'; // 设置 div 的 class
+            listintroduce.className = 'wenbjieshao'; // 设置文本介绍 class
+            // 文本内容
+            listTitle.textContent = item.name; // 标题内容
+            listintroduce.textContent = item.text // 文本介绍
+            listDate.textContent = item.time// 日期
+            // 添加文本
+            listElement.appendChild(listItem);// 将新的列表项添加到列表中
+            listItem.appendChild(listTitle);// 文本标题
+            listItem.appendChild(listintroduce);// 文本介绍
+            listItem.appendChild(listDate);// 日期
+        }
+    })
+}
--- yemian/zhuye.js
+++ yemian/zhuye.js
@@ -1,51 +1,38 @@
 import './zhuye.css';
+import { duqvmdwenjian,duqvmdname } from './zhuycounter'
 // 读取md文件库
 import { marked } from 'marked';
-import fs from 'fs'
-import path from 'path';
 
-document.querySelector('#add').innerHTML = `
-    <div id="wenzhang">
-        
+duqvmdwenjian().then(count => {
+    let shuliang = count
+    document.querySelector('#add').innerHTML = `
+    <div id="zuobian" class="zuobian_yangshi">
+        <img class="touxing" src="../img/touxing.jpg">
+        <div class="wenzhi">
+            <span>文章篇幅</span>
+            <span>${shuliang}篇</span>
+            <span>铭记</span>
+            <span>2016.6.6</span>
+        </div>
+    </div>
+    <div id="youbian" class="youbian_yangshi">
+        <div id="wenzhang" class="wenzhang">
+            
+        </div>
     </div>
 `
-// // 假设你的 Markdown 文件在构建后的 public 文件夹中，并且可以通过相对路径访问  
-const markdownFilePath = '/biji/bijiweiz.json'; // 注意这里的路径可能是相对于构建后的根目录  
 
-// fetch('/biji/bijiweiz.json').then(response => response.json()).then(data => {
-//     console.log(data, 'data');
-//     // 清空之前的列表内容（如果存在）  
-//     let listElement = document.getElementById("wenzhang");
-
-//     for (const item of data) {
-//         let listItem = document.createElement('li');
-//         listItem.textContent = item.name; // 设置列表项的内容为文件名 
-//         console.log(item.name, 'item.name');
-//         // 将新的列表项添加到列表中  
-//         listElement.appendChild(listItem);
-//     }
-// })
-
-fetch('/biji/bijiweiz.json').then(response => response.json()).then(data => {
-    console.log(data, 'data');
-    // 清空之前的列表内容（如果存在）  
-    let listElement = document.getElementById("wenzhang");
-
-    for (const item of data) {
-        let listItem = document.createElement('li');
-        listItem.textContent = item.name; // 设置列表项的内容为文件名 
-        console.log(item.name, 'item.name');
-        // 将新的列表项添加到列表中  
-        listElement.appendChild(listItem);
-    }
 })
-fetch(markdownFilePath)
-    .then(response => response.text())
-    .then(markdownText => {
-        console.log(markdownText);
-        // const html = marked(markdownText);
-        // // 将 HTML 插入到 DOM 中
-        // document.getElementById('wenzhang').innerHTML = html;
-    })
+
+duqvmdname()
+
+// fetch(markdownFilePath)
+//     .then(response => response.text())
+//     .then(markdownText => {
+//         console.log(markdownText);
+//         // const html = marked(markdownText);
+//         // // 将 HTML 插入到 DOM 中
+//         // document.getElementById('wenzhang').innerHTML = html;
+//     })
 
 
